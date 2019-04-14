f9c98e48a8f26fbf49d4ed5ca91db39ac6fb9b6e
JackM10 <jack.vpl@gmail.com>
2019-04-14 22:31:56 +0300

    working version for simple zip brutforcer after refactoring

--- ZIPbrutforcer/Program.cs
+++ ZIPbrutforcer/Program.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using Ionic.Zip;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Ionic.Zip;
-using Ionic.Zlib;
 
 namespace ZIPbrutforcer
 {
@@ -12,44 +11,26 @@ namespace ZIPbrutforcer
     {
         static void Main()
         {
-            string ExistingZipFile = "1.zip";
-            string BaseDirectory = "D:\\tmp";
+            string zipFileName = "1.zip";
+            string BaseDirectory = "D:\\Temp";
 
-            var allLines = File.ReadAllLines("1.txt");
-            var totalCount = allLines.LongLength;
-            var counter = 0L;
+            var allLinesPassword = File.ReadAllLines("D:\\Temp\\10m.txt");
+            var totalCount = allLinesPassword.LongLength;
 
-            Parallel.For(0, totalCount, i =>
+            using (ZipFile zip = ZipFile.Read(Path.Combine(BaseDirectory, zipFileName)))
             {
-                Interlocked.Increment(ref counter);
-                var password = allLines[i];
-                try
+                Parallel.For(0, allLinesPassword.Length, i =>
                 {
-                    using (var stream = new FileStream(ExistingZipFile, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan))
-
-                    using (ZipFile zip = ZipFile.Read(stream))
+                    try
                     {
-                        ZipEntry e = zip["DELtrial_14.exe"];
-                        e.ExtractWithPassword(Path.Combine(BaseDirectory, Thread.CurrentThread.ManagedThreadId.ToString()), ExtractExistingFileAction.DoNotOverwrite, password);
+                        zip.Password = allLinesPassword[i];
+                        zip.ExtractAll(BaseDirectory, ExtractExistingFileAction.DoNotOverwrite);
                     }
-                }
-                catch (BadPasswordException)
-                {
-                    Console.WriteLine($"Pass # {counter} is not Ok!");
-                }
-                catch (ZlibException)
-                {
-                    Console.WriteLine("Bad Zlib happen");
-                }
-                catch (BadStateException)
-                {
-                    Console.WriteLine("Bad State happen");
-                }
-                catch (BadReadException)
-                {
-                    Console.WriteLine("Bad read happen");
-                }
-            });
+                    catch (BadPasswordException)
+                    {
+                    }
+                });
+            }
         }
     }
 }
